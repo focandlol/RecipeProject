955d710633426763332078ca5f5dc2cf61103114
focandlol <focandlol@naver.com>
2025-01-30 13:14:49 +0900

    feat : TagEntity 추가 및 name 컬럼에 인덱스 적용

---
+++ src/main/java/focandlol/recipeproject/tag/entity/TagEntity.java
@@ -0,0 +1,42 @@
+package focandlol.recipeproject.tag.entity;
+
+import focandlol.recipeproject.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tag", indexes = @Index(name = "idx_tag_name", columnList = "name"))
+public class TagEntity extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "tag_id")
+  private Long id;
+
+  @Column(unique = true, nullable = false, length = 20)
+  private String name;
+
+  @Column(nullable = false)
+  @ColumnDefault("0")
+  private Long count;
+
+  @PrePersist
+  public void prePersist() {
+    if(count == null) {
+      count = 0L;
+    }
+  }
+}
