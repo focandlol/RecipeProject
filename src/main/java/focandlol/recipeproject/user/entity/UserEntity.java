a8fbbd8d7ae6999535d615e67dd334353cd78e6f
focandlol <focandlol@naver.com>
2025-01-30 12:40:56 +0900

    refactor : UserEntity 테이블명 변경

--- src/main/java/focandlol/recipeproject/user/entity/UserEntity.java
+++ src/main/java/focandlol/recipeproject/user/entity/UserEntity.java
@@ -9,6 +9,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,6 +23,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "user")
 public class UserEntity extends BaseEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
